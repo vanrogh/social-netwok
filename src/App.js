e020046d21b7d709bd62a8906802adf933e534ff
vanrogh <k.elaman2002@gmail.com>
2023-11-13 22:18:17 +0600

    Шаг 1. Верстка, grid css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src="https://i.pinimg.com/1200x/0d/cf/b5/0dcfb548989afdf22afff75e2a46a508.jpg" />
       </header>
+      <nav className='nav'>
+        <div>
+          <a href="#">Profile</a>
+        </div>
+        <div>
+          <a href="#">Messages</a>
+        </div>
+        <div>
+          <a href="#">News</a>
+        </div>
+        <div>
+          <a href="#">Music</a>
+        </div>
+        <div>
+          <a href="#">Settings</a>
+        </div>
+
+      </nav>
+      <div className='content'>
+        <div>
+          <img src="https://learnopencv.com/wp-content/uploads/2021/04/image-15.png" />
+        </div>
+        <div>
+          ava + decription
+        </div>
+        <div>
+          My Posts
+          <div>
+            New Post
+          </div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
